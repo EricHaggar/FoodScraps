dac46eb2b46e0719a2b6b408978b831661435bfe
EricHaggar <erichaggar@hotmail.com>
2018-10-21 04:34:47 -0400

    Deleted unneeded files

--- scripts/webscrapppp.py
+++
@@ -1,9 +0,0 @@
-#from selenium import webdriver
-#from selenium.common.expceptions import NoSuchElmentException
-#from selenium import *
-#import xlwt
-import webbrowser
-new=2
-url= "http://www.healthycanadians.gc.ca/recall-alert-rappel-avis/search-recherche/simple?s=%22Maple+Leaf+Foods%22&plain_text=%22Maple+Leaf+Foods%22&js_en=0&f_mc=1"
-webbrowser.open(url,new=new)
-
