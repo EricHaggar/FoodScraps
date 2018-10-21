61737be15c0ba3fdb07c46cde1326a4aff690e58
Aymen <amprodz15@gmail.com>
2018-10-21 04:17:44 -0400

    Changed imlementation/ no need for excel files

--- scripts/glassdoor_script.py
+++ scripts/glassdoor_script.py
@@ -3,6 +3,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium import *
 import xlwt
 
+companies = ["Neilson", "McCain Foods"]
+
+
 
 def findURL(query):
 
@@ -13,26 +16,14 @@ def findURL(query):
   
         # to search 
       
-    for j in search(query, tld="com", lang='en', num=1, stop=1, pause=1): 
+    for j in search(query, tld="ca", lang='en', num=2, stop=1, pause=1): 
         url = j
 
     return j
 
 
-def scrape(base_url, workbook, name, ss):
-    worksheet = workbook.add_sheet("sheet1", cell_overwrite_ok=True)
-    worksheet.write(0, 1, "Title")
-    worksheet.write(0, 2, "Date Written")
-    worksheet.write(0, 3, "Rating")
-    worksheet.write(0, 4, "Current/Former")
-    worksheet.write(0, 5, "Job Title")
-    worksheet.write(0, 6, "Location")
-    worksheet.write(0, 7, "Recommendation?")
-    worksheet.write(0, 8, "Outlook")
-    worksheet.write(0, 9, "Main Text")
-    worksheet.write(0, 10, "Pros")
-    worksheet.write(0, 11, "Cons")
-    worksheet.write(0, 12, "Advice to management")
+def scrape(base_url, workbook, name):
+    
     # setting first url
     url = base_url
     driver = webdriver.Chrome()
@@ -44,8 +35,7 @@ def scrape(base_url, workbook, name, ss):
     password = driver.find_element_by_name("password")
     password.send_keys("glassdoor")
     driver.find_element_by_xpath("//*[@class='gd-btn gd-btn-1 fill']").click()
-    sample_size = ss
-    # after login
+    
     try:
         # go to first link
         driver.get(url)
@@ -58,70 +48,27 @@ def scrape(base_url, workbook, name, ss):
                 page += char
         # this is the current page we are on
         page = int(page)
-        counter = 0
-        loop_count = 0
-        sample_size = int(sample_size)
-        while loop_count < sample_size/10:
-            # search the company
-            # ~~~~~~~~~~~~~~~~~~~TITLE OF REVIEW~~~~~~~~~~~~~~~~~~~~~~
-            titles = driver.find_elements_by_class_name("reviewLink")
-            titlearr = []
-            for title in titles:
-                counter += 1
-                titlearr.append(title.text)
+        ratings = driver.find_element_by_class_name("ratingNum").text
+        print(ratings)
 
-            # ~~~~~~~~~~~~~~~~~~~~TIMESTAMPS~~~~~~~~~~~~~~~~~~~~~~~
-            timestamps = driver.find_elements_by_class_name("floatLt")
-            datelist = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            true_timestamps = []
-            for datetime in timestamps:
-                if datetime.text[:3] in datelist:
-                    true_timestamps.append(datetime.text)
-                else:
-                    true_timestamps.append("no-date")
-
-
-
-            # ~~~~~~~~~~~~~~~~~~~RATINGS~~~~~~~~~~~~~~~~~~~~~~~~~
-            ratings = driver.find_elements_by_class_name("value-title")
-            ratingarr = []
-            for rating in ratings:  # NEED TO REMOVE THE FIRST ONE BECAUSE ITS GETTING OVERALL RATING
-                ratingarr.append(rating.get_attribute('title'))
-            if int(page) == 1:
-                del ratingarr[0]
-
-    
-            for index in range(0, len(titlearr)):
-                print("INDEX IS: ", index)
-                print("ROW: ", int(page)*10+index)
-                
-                worksheet.write(int(page)*10+index, 3, ratingarr[index])
-                loop_count += 1
-            try:
-                found = driver.find_element_by_css_selector("#FooterPageNav > div > ul > li.page.current.last > span")
-                print(found.text)
-                workbook.save(name)
-                break
-
-            except NoSuchElementException:
-                page += 1
-                driver.get(basic+str(page)+".htm")
-                print("clicked page", str(page))
-
-        workbook.save(name)
-        print("done")
+        
 
     except NoSuchElementException:
         workbook.save(name)
         print("finished")
 
-def mainScraper(a,i):
-    company_info = xlwt.Workbook(encoding="utf-8")
-    name = "output"+str(i)+".xls"
-    sample_size = "10"
-    url_link = findURL(a+" glassdoor")
-    url_link = url_link[:-4] + "_P1.htm"
-    scrape(url_link, company_info, name, sample_size)
+    
+def mainScraper():
+    for i in range(len(companies)):
+        company_info = xlwt.Workbook(encoding="utf-8")
+        name = "output"+str(i)+".xls"
+        sample_size = "20"
+        url_link = findURL(companies[i]+" glassdoor.ca/Overview")
+        print (url_link)
+        scrape(url_link, company_info, name)
+
+    
+    
     
 
 
--- scripts/master.py
+++ scripts/master.py
@@ -1,9 +1,6 @@
 import os
 import glassdoor_script 
 
-companies = ["Neilson", "McCain Foods"]
-
-for i in range(len(companies)):
-        glassdoor_script.mainScraper(companies[i], i)
+glassdoor_script.mainScraper()
 
 
