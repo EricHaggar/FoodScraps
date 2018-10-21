7f4218b0f441d42c5e4d4dc3f78971079e59ce28
EricHaggar <erichaggar@hotmail.com>
2018-10-21 04:22:53 -0400

    Added list of companies to analyze

---
+++ scripts/companies.py
@@ -0,0 +1,7 @@
+#List of companies to analyze
+
+Companies = ["McCain Foods", "Cara Operations", "Nature's Path", "Olymel", "Purity Factories", "Rogers Sugar", "Saputo", "Naya Waters", "Sobeys", "Daiya", "SunOpta",
+             "Organic Meadow Cooperative", "Agropur", "Première Moisson", "Bothwell Cheese", "Dan-D Foods", "Maple Leaf Foods", "Kraft Heinz", "M&M Food Market",
+             "PepsiCo", "Dare Foods", "Flowers Foods", "Pinnacle Foods", "Reser's Fine Foods", "Kawartha Dairy Company", "Nestle", "Saputo Inc", "Just Us!",
+             "Laura Secord Chocolates", "Voortman Cookies", "Lester's Foods Ltd.", "Earth's Own Food Company", "Canyon Creek Food Company", "Cara Operations",
+             "Chapman's", "Metro Inc."]
