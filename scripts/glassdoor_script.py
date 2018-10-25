429589f7d5994a44acd989c76463b207588dc3eb
EricHaggar <erichaggar@hotmail.com>
2018-10-25 04:05:05 -0400

    Updated PowerPoint presentation and added user story description

--- scripts/glassdoor_script.py
+++ scripts/glassdoor_script.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium import *
 import xlwt
 
-companies = ['"McCain Foods"', '"Cara Operations"', '"'+"Nature's Path"+'"', '"Olymel"', '"Purity Factories"', '"Rogers Sugar"', '"Saputo"', '"Naya Waters"', '"Sobeys"', "Daiya", "SunOpta",
+companies = ['"McCain Foods"', '"Cara Operations"', '"'+"Nature's Path"+'"', '"Purity Factories"', '"Saputo"', '"Sobeys"', "Daiya", "SunOpta",
              '"Organic Meadow Cooperative"', '"Agropur"', '"Première Moisson"', '"Bothwell Cheese"', '"Dan-D Foods"', '"Maple Leaf Foods"', '"Kraft Heinz"', '"M&M Food Market"',
              '"PepsiCo"', '"Dare Foods"', '"Flowers Foods"', '"Pinnacle Foods"', '"'+"Reser's Fine Foods"+'"', '"Kawartha Dairy Company"', '"Nestle"', '"Saputo Inc"', "Just Us!",
              '"Laura Secord Chocolates"', '"Voortman Cookies"', '"'+"Lester's Foods Ltd."+'"', '"'+"Earth's Own Food Company"+'"', '"Canyon Creek Food Company"', "Cara Operations",
